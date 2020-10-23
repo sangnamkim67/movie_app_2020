98a49fc1345874f79881daec15553fddfc0438f4
sangnamkim <56857580+sangnamkim67@users.noreply.github.com>
2020-10-23 17:00:42 +0900

    Updates

---
+++ precache-manifest.8d72de36a82436de27ed5da95348f9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4e739680c178d8b4de3bd748c9bc15",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e6a410535e1deab57f7c",
+    "url": "/movie_app_2020/static/css/main.fa07f1f8.chunk.css"
+  },
+  {
+    "revision": "8998b155a8923305826e",
+    "url": "/movie_app_2020/static/js/2.3cea1fbe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.3cea1fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a410535e1deab57f7c",
+    "url": "/movie_app_2020/static/js/main.c0536c37.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
